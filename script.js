e173cb67a510b0345fb9cd81cd0e4bd0d1770790
Nikil <nikil24v@gmail.com>
2026-07-15 12:07:38 +0530

    Add task funtion added

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//retruieving from the local storage 
-//And intializing empty arrays and variables 
-
+// Retrieving from localStorage and initializing empty arrays/variables
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 const todoinput = document.getElementById("todoInput");
 const todolist = document.getElementById("todolist");
@@ -8,29 +6,40 @@ const todocount = document.getElementById("todocount");
 const addbtn = document.querySelector(".add-btn");
 const deletebtn = document.querySelector(".delete-btn");
 
-
-//intialize the page and starts listining
-
-document.addEventListener("DOMContentLoaded", function() {
-    addbtn.addEventListener("click", addtask());
-    todoinput.addEventListener("keydown", function(event){
-        if(event.key === "Enter") {
+// Initialize the page and start listening
+document.addEventListener("DOMContentLoaded", function () {
+    addbtn.addEventListener("click", addtask);
+    todoinput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
             event.preventDefault();
             addtask();
         }
     });
-    deletebtn.addEventListener("click",deletealltasks());
+    deletebtn.addEventListener("click", deletealltasks);
     displaytasks();
 });
 
 function addtask() {
-    
+    const newtask = todoinput.value.trim();
+    if(newtask !== "")
+    {
+        todo.push( {
+            text:newtask,
+            disabled:false,
+        })
+        savetolocalstorage();
+        todoinput.value = "";
+        displaytasks();
+    }
 }
 
 function deletealltasks() {
-
 }
 
 function displaytasks() {
-    
+
+}
+
+function savetolocalstorage() {
+    localStorage.setItem("todo",JSON.stringify(todo));
 }
